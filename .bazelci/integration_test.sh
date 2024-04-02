9646b6ff6f8e99b5acd3819e69d23d06404f06a5
George Gensure <werkt0@gmail.com>
2024-04-02 11:47:12 -0400

    Some incremental improvements to ci testing
    Shut down the redis container on integration complete
    Start redis after building source
    Inspire bazel binary from env vars
    Use ubuntu 24.04 for integration testing
    Only support python3 for coverage generation
    Manipulate coverage hosting port

--- .bazelci/integration_test.sh
+++ .bazelci/integration_test.sh
@@ -4,12 +4,17 @@
 # All of the needed buildfarm services are initialized (redis, server, worker).
 # We ensure that the system can build a set of bazel targets.
 
+# Build a container for buildfarm services
+if [ -z "$BAZEL" ]
+then
+    BAZEL=bazel
+fi
+BAZEL_BINARY=$(which $BAZEL)
+cp $BAZEL_BINARY bazel
+docker build -t buildfarm .
+
 # Run redis container
 docker run --rm -d --name buildfarm-redis --network host redis:7.2.4 --bind localhost
-
-# Build a container for buildfarm services
-cp `which bazel` bazel
-docker build -t buildfarm .
 
 # Start the servies and do a test build
 docker run --rm \
